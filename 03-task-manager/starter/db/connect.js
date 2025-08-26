d19376bf715a7cc69b5e0baea12ed6d861544d8d
Victor Hernandez <hernandezv0110@gmail.com>
2025-08-25 22:00:47 -0700

    Completed week5 tasks

---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,13 @@
+const e = require('express');
+const mongoose = require('mongoose');
+
+const connectDB = (url) => {
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+} 
+
+module.exports = connectDB;
